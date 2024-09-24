f88abad972ba95a1e07626953bcced64a459afee
ZairGamer <111931453+ZairGamer@users.noreply.github.com>
2024-09-23 22:33:24 -0700

    Add files via upload

---
+++ Hola mundo C++ y raptor/holamundo2.0.cxx
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+	printf("Hola Mundo");		
+	printf("\n Hola mundo 2"); // Salto de linea //
+	printf("\n Hola Mundo 3 \t Hola mundo 4"); // Hace una distancia entre los 2 textos //	
+	printf("\n\n\n Hola mundo 5 \a"); // Salta 3 lineas y envia un mensaje de alerta con sonido //
+	printf("\n \"Hola mundo 6\""); // Poner simbolos al texto //
+}
