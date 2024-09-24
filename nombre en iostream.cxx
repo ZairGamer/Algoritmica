256d980f26ae07991e5af4b24f89880c67135cf7
ZairGamer <111931453+ZairGamer@users.noreply.github.com>
2024-09-23 22:21:05 -0700

    Edad y nombre con iostream solo c++

---
+++ nombre en iostream.cxx
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	char nombre[20];
+	cout << "Introduzca su nombre: ";
+	cin >> nombre;
+	
+	cout << "Hola " << nombre << ", Buenos dias" ;
+	
+	return 0;
+}
